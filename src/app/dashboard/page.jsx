d8eb883be3054c7d295eda4580335c87ea9559fe
donnysppl <donny@sppl.ind.in>
2023-08-08 14:35:17 +0530

    updating github repo on 08-08-2023 : 14:35

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useEffect } from 'react'
+import bwipjs from 'bwip-js';
+
 
 
 export default function Dashboard() {
@@ -23,27 +25,64 @@ export default function Dashboard() {
         })
     };
     authbar();
-    const authbar2 = async () => {
-      await fetch('https://staging.ekartlogistics.com/auth/token', {
-        method: 'POST',
-        mode:'no-cors',
-        headers: {
-          'Authorization': 'Basic cGxhc3Ryb25pY3NsYXJnZTpkdW1teUtleQ==',
-          'Content-Type': 'application/json',
-          'HTTP_X_MERCHANT_CODE': 'SPL',
-        }
-      }).then(res => res.json())
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        })
-    };
-    authbar2();
+
+
+
+  //   const authbar2 = async () => {
+  //     await fetch('https://staging.ekartlogistics.com/auth/token', {
+  //       method: 'POST',
+  //       mode:'no-cors',
+  //       headers: {
+  //         'Authorization': 'Basic cGxhc3Ryb25pY3NsYXJnZTpkdW1teUtleQ==',
+  //         'Content-Type': 'application/json',
+  //         'HTTP_X_MERCHANT_CODE': 'SPL',
+  //       }
+  //     }).then(res => res.json())
+  //       .then(res => {
+  //         console.log(res);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       })
+  //   };
+  //   authbar2();
   }, [])
 
+  // useEffect(() => {
+  //   try {
+  //     // Th
+  //     let canvas = document.createElement('canvas');
+  //     bwipjs.toCanvas(canvas, {
+  //       bcid: 'code128',       // Barcode type
+  //       text: '0123456789',    // Text to encode
+  //       scale: 3,               // 3x scaling factor
+  //       height: 10,              // Bar height, in millimeters
+  //       includetext: true,            // Show human-readable text
+  //       textxalign: 'center',        // Always good to set this
+  //     });
+  //     document.getElementById('my-img').src = canvas.toDataURL('image/png');
+  //   } catch (e) {
+  //     // `e` may be a string or Error object
+  //   }
+  // }, [])
+
+  // let canvas = window.document.createElement('canvas');
+  //     bwipjs.toCanvas(canvas, {
+  //       bcid: 'code128',       // Barcode type
+  //       text: '0123456789',    // Text to encode
+  //       scale: 3,               // 3x scaling factor
+  //       height: 10,              // Bar height, in millimeters
+  //       includetext: true,            // Show human-readable text
+  //       textxalign: 'center',        // Always good to set this
+  //     });
+  //     window.document.getElementById('my-img').src = canvas.toDataURL('image/png');
+
+
   return (
-    <div>Dashboard</div>
+    <div>
+      {/* <canvas id="mycanvas"></canvas>
+      <img id='my-img'/> */}
+
+    </div>
   )
 }
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@
 
 export default function Loader() {
   return (
-    <div className="loader-outer">
+    <div className="loader-outer min-h-[500px] min-w-[100px]">
         <div className="spinner"></div>
     </div>
   )
