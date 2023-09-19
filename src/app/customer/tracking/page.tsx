d1eadb81a021d9383409b2d170967a52f4950c22
donnysppl <donny@sppl.ind.in>
2023-09-19 15:20:51 +0530

    adding logo and favicon

--- src/app/customer/tracking/page.tsx
+++ src/app/customer/tracking/page.tsx
@@ -4,6 +4,7 @@ import { HistoryTrack, Tracking } from '@/interface/interface';
 import moment from 'moment';
 import React, { useState } from 'react';
 import toast from 'react-hot-toast';
+import Image from 'next/image'
 
 interface CustomerTrackState {
     phone: string,
@@ -68,60 +69,70 @@ export default function CustomerTracking() {
     }
 
     return (
-        <div className='flex items-center justify-center min-h-screen p-2'>
+        <div className='flex items-center justify-center min-h-screen p-2 flex-col'>
+            <div className="logo flex p-4 mb-3">
+                <Image className='mx-auto'
+                    src={'/assets/img/sppl-logo.png'}
+                    width={200}
+                    height={200}
+                    alt="logo" layout="fixed"
+                />
+            </div>
             <main className='relative max-w-2xl w-full mx-auto border border-gray-700 shadow-xl rounded-2xl md:p-8 p-4'>
+
                 <div>
-                <h2 className='mb-5'>Track Your Order</h2>
-                <div className={`relative ${loader ? 'w-full h-[500px]' : null}`}>
-                    {
-                        loader ? <Loader /> :
-                            <>
-                                <div className="customer-tracking-form">
-                                    <form onSubmit={onCustomerTrack} >
-                                        <div className="mb-4">
-                                            <label htmlFor="phone" className="form-label ">Phone Number</label>
-                                            <input type="text" name="phone" className="form-inp" required placeholder="Phone Number"
-                                                onChange={(e) => setcustTrackInp({ ...custTrackInp, phone: e.target.value })} />
-                                        </div>
-                                        <div className="mb-4">
-                                            <label htmlFor="trackingid" className="form-label ">Tracking ID</label>
-                                            <input type="text" name="trackingid" className="form-inp" required placeholder="Tracking ID"
-                                                onChange={(e) => setcustTrackInp({ ...custTrackInp, trackingid: e.target.value })} />
-                                        </div>
-                                        <button type='submit' className='btn-main mt-3'>Submit</button>
-                                    </form>
-                                </div>
 
-                                {
-                                    showTrack ?
-                                        <div className="customer-track-data p-5">
-                                            {
-                                                trackingresHis.length ?
-                                                    <>
-                                                        <h5 className='mb-3'>Expected Date : {moment(trackResponse?.expected_delivery_date).format('MMMM Do YYYY')} </h5>
-                                                        {trackingresHis?.map((item, index) => {
-                                                            return (
-                                                                <div key={index} className='border-l border-gray-500 p-3 ps-5 see-details-item'>
-                                                                    <div>{item.description} <span className='text-gray-500 text-sm'>{moment(item.event_date).format('MMMM Do YYYY, h:mm:ss a')}</span></div>
-                                                                    <div className='text-gray-500 text-sm'>Status : {item.status}</div>
-                                                                    <div className='text-gray-500 text-sm'>Hub Name : {item.hub_name}</div>
-                                                                </div>
-                                                            )
-                                                        })}
-                                                    </> : <div className='text-center text-sm mt-5'>
-                                                        Ekart data not created, Please wait for 12 to 24 hours to create a ekart shipment data
-                                                    </div>
-                                            }
+                    <h2 className='mb-5'>Track Your Order</h2>
+                    <div className={`relative ${loader ? 'w-full h-[500px]' : null}`}>
+                        {
+                            loader ? <Loader /> :
+                                <>
+                                    <div className="customer-tracking-form">
+                                        <form onSubmit={onCustomerTrack} >
+                                            <div className="mb-4">
+                                                <label htmlFor="phone" className="form-label ">Phone Number</label>
+                                                <input type="text" name="phone" className="form-inp" required placeholder="Phone Number"
+                                                    onChange={(e) => setcustTrackInp({ ...custTrackInp, phone: e.target.value })} />
+                                            </div>
+                                            <div className="mb-4">
+                                                <label htmlFor="trackingid" className="form-label ">Tracking ID</label>
+                                                <input type="text" name="trackingid" className="form-inp" required placeholder="Tracking ID"
+                                                    onChange={(e) => setcustTrackInp({ ...custTrackInp, trackingid: e.target.value })} />
+                                            </div>
+                                            <button type='submit' className='btn-main mt-3'>Submit</button>
+                                        </form>
+                                    </div>
 
-                                        </div> : null
-                                }
-                            </>}
+                                    {
+                                        showTrack ?
+                                            <div className="customer-track-data p-5">
+                                                {
+                                                    trackingresHis.length ?
+                                                        <>
+                                                            <h5 className='mb-3'>Expected Date : {moment(trackResponse?.expected_delivery_date).format('MMMM Do YYYY')} </h5>
+                                                            {trackingresHis?.map((item, index) => {
+                                                                return (
+                                                                    <div key={index} className='border-l border-gray-500 p-3 ps-5 see-details-item'>
+                                                                        <div>{item.description} <span className='text-gray-500 text-sm'>{moment(item.event_date).format('MMMM Do YYYY, h:mm:ss a')}</span></div>
+                                                                        <div className='text-gray-500 text-sm'>Status : {item.status}</div>
+                                                                        <div className='text-gray-500 text-sm'>Hub Name : {item.hub_name}</div>
+                                                                    </div>
+                                                                )
+                                                            })}
+                                                        </> : <div className='text-center text-sm mt-5'>
+                                                            Ekart data not created, Please wait for 12 to 24 hours to create a ekart shipment data
+                                                        </div>
+                                                }
+
+                                            </div> : null
+                                    }
+                                </>}
+
+                    </div>
 
                 </div>
 
-                </div>
 
-                
 
             </main>
         </div>
