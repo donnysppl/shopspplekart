63e65f943523f09f7de24af4b0a3208a8cd20464
donnysppl <donny@sppl.ind.in>
2024-02-13 15:28:18 +0530

    some changes in api

--- src/app/api/orderdata/route.ts
+++ src/app/api/orderdata/route.ts
@@ -1,31 +1,87 @@
-import fetch from 'node-fetch';
 import { NextRequest, NextResponse } from "next/server";
+import { MongoClient } from 'mongodb';
 
 export async function GET(req: NextRequest) {
+    const url = process.env.MONGODB_URL_ORDER as string;
+
+    const client = new MongoClient(url);
+
     try {
-        const apifetch = await fetch(`${process.env.NEXT_PUBLIC_ORDER_LINK}/api/ekartcon/orderlist`, {
-            method: 'GET',
-            headers: {
-                'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_ALLOW_ORIGIN}`,
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Content-Type': 'application/json',
-                'Cache-Control': 'no-store',
-            },
+        // Connect to the MongoDB server
+        await client.connect();
 
-        });
-        const responseData = await apifetch.json() as any;
+        // Specify the database and collection
+        const db = client.db('shopsppl');
+        const collection = db.collection('productorders');
 
+        // Fetch data from the collection
+        const result = await collection.find({}).toArray();
+
+        // Send the retrieved data as the API response
+        //   res.status(200).json(result);
         return NextResponse.json({
             status: 200,
             message: 'Success',
             success: true,
-            result: responseData.result as any,
+            result: result,
         }, { status: 200 })
     } catch (error) {
+        //   console.error('Error fetching data:', error);
         console.log(error)
         return NextResponse.json({
             status: 500,
             message: 'Somthing went wrong ' + error,
         }, { status: 500 })
+    } finally {
+        // Close the MongoDB client
+        await client.close();
     }
+
+
+
+    // try {
+    //     const url = process.env.MONGODB_URL_ORDER as string;
+
+    //     let resultData:any[] = [];
+    //     MongoClient.connect(url, function(err:any, db:any) {
+    //         if (err) throw err;
+    //         var dbo = db.db("shopsppl");
+    //         //Find all documents in the customers collection:
+    //         dbo.collection("productorders").find({}).toArray(function(err:any, result:any) {
+    //           if (err) throw err;
+    //           resultData = result;
+    //           db.close();
+    //         });
+    //       });
+
+
+    //     // const apifetch = await fetch(`${process.env.NEXT_PUBLIC_ORDER_LINK}/api/ekartcon/orderlist`, {
+    //     //     method: 'GET',
+    //     //     headers: {
+    //     //         'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_ALLOW_ORIGIN}`,
+    //     //         'Access-Control-Allow-Headers': 'Content-Type',
+    //     //         'Content-Type': 'application/json',
+    //     //         'Cache-Control': 'no-store',
+    //     //     },
+
+    //     // });
+    //     // const responseData = await apifetch.json() as any;
+
+
+
+
+
+    //     return NextResponse.json({
+    //         status: 200,
+    //         message: 'Success',
+    //         success: true,
+    //         result: resultData as any,
+    //     }, { status: 200 })
+    // } catch (error) {
+    //     console.log(error)
+    //     return NextResponse.json({
+    //         status: 500,
+    //         message: 'Somthing went wrong ' + error,
+    //     }, { status: 500 })
+    // }
 }
